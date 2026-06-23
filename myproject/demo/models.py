e6a0beb923d628dffc25706347ecadcd8c35b9a8
Alok Kumar Nayak <alokkumar.nayak2009@gmail.com>
2026-06-23 09:54:34 +0530

    '23-06-2026'

---
+++ myproject/demo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    rollno = models.IntegerField()
+    
+
